417465cc4d059a5d14a9bde90f3bf5557de28c7b
xream <xream@icloud.com>
2024-03-08 18:39:29 +0800

    [!] 在 Sub-Store 中添加流量信息节点时, 兼容远程和本地合并的情况, 排除设置了不查询订阅信息的链接

--- surge/modules/sub-store-scripts/sub-info/node.js
+++ surge/modules/sub-store-scripts/sub-info/node.js
@@ -1,27 +1,56 @@
 async function operator(proxies = [], targetPlatform, env) {
   const { parseFlowHeaders, getFlowHeaders, flowTransfer } = flowUtils
-  const {
-    expires,
-    total,
-    usage: { upload, download },
-  } = parseFlowHeaders(await getFlowHeaders(env.source[proxies[0].subName].url))
-  const date = expires ? new Date(expires * 1000).toLocaleDateString() : ''
+  const sub = env.source[proxies[0].subName]
+  const localOnly = sub.source === 'local' && !['localFirst', 'remoteFirst'].includes(sub.mergeSources)
 
-  const current = upload + download
-  const currT = flowTransfer(Math.abs(current))
-  currT.value = current < 0 ? '-' + currT.value : currT.value
-  const totalT = flowTransfer(total)
-  // 获取 proxies 的最后一项
-  const node = proxies[proxies.length - 1] || {
-    type: 'ss',
-    server: '1.0.0.1',
-    port: 80,
-    cipher: 'aes-128-gcm',
-    password: 'password',
+  if (!localOnly && sub.url) {
+    let url = `${sub.url}`
+      .split(/[\r\n]+/)
+      .map(i => i.trim())
+      .filter(i => i.length)?.[0]
+
+    let $arguments = {}
+    const rawArgs = url.split('#')
+    url = url.split('#')[0]
+    if (rawArgs.length > 1) {
+      try {
+        // 支持 `#${encodeURIComponent(JSON.stringify({arg1: "1"}))}`
+        $arguments = JSON.parse(decodeURIComponent(rawArgs[1]))
+      } catch (e) {
+        for (const pair of rawArgs[1].split('&')) {
+          const key = pair.split('=')[0]
+          const value = pair.split('=')[1]
+          // 部分兼容之前的逻辑 const value = pair.split('=')[1] || true;
+          $arguments[key] = value == null || value === '' ? true : decodeURIComponent(value)
+        }
+      }
+    }
+    if (!$arguments.noFlow) {
+      const {
+        expires,
+        total,
+        usage: { upload, download },
+      } = parseFlowHeaders(await getFlowHeaders(url))
+      const date = expires ? new Date(expires * 1000).toLocaleDateString() : ''
+
+      const current = upload + download
+      const currT = flowTransfer(Math.abs(current))
+      currT.value = current < 0 ? '-' + currT.value : currT.value
+      const totalT = flowTransfer(total)
+      // 获取 proxies 的最后一项
+      const node = proxies[proxies.length - 1] || {
+        type: 'ss',
+        server: '1.0.0.1',
+        port: 80,
+        cipher: 'aes-128-gcm',
+        password: 'password',
+      }
+      proxies.unshift({
+        ...node,
+        name: `流量信息: ${currT.value} ${currT.unit} / ${totalT.value} ${totalT.unit} ${date}`,
+      })
+    }
   }
-  proxies.unshift({
-    ...node,
-    name: `流量信息: ${currT.value} ${currT.unit} / ${totalT.value} ${totalT.unit} ${date}`,
-  })
+
   return proxies
 }
