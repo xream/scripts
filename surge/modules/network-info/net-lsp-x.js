879dbbcb72e9f0a99ad242475823bdee50bf8c4a
xream <xream@icloud.com>
2025-07-03 18:32:56 +0800

    feat: 网络信息增加百度接口

--- surge/modules/network-info/net-lsp-x.js
+++ surge/modules/network-info/net-lsp-x.js
@@ -328,7 +328,7 @@ async function getDirectRequestInfo({ PROXIES = [] } = {}) {
   const { CN_IP, CN_INFO } = await getDirectInfo(undefined, $.lodash_get(arg, 'DOMESTIC_IPv4'))
   const { POLICY } = await getRequestInfo(
     new RegExp(
-      `cip\\.cc|for${keyb}\\.${keya}${bay}\\.cn|rmb\\.${keyc}${keyd}\\.com\\.cn|api-v3\\.${keya}${bay}\\.cn|ipservice\\.ws\\.126\\.net|api\\.bilibili\\.com|api\\.live\\.bilibili\\.com|myip\\.ipip\\.net|ip\\.ip233\\.cn|ua${keye}\\.wo${keyf}x\\.cn|ip\\.im|ips\\.market\\.alicloudapi\\.com|api\\.ip\\.plus|appc\\.${keyg}${keyh}\\.com|dashi\\.163\\.com`
+      `cip\\.cc|for${keyb}\\.${keya}${bay}\\.cn|rmb\\.${keyc}${keyd}\\.com\\.cn|api-v3\\.${keya}${bay}\\.cn|ipservice\\.ws\\.126\\.net|api\\.bilibili\\.com|api\\.live\\.bilibili\\.com|myip\\.ipip\\.net|ip\\.ip233\\.cn|ua${keye}\\.wo${keyf}x\\.cn|ip\\.im|ips\\.market\\.alicloudapi\\.com|api\\.ip\\.plus|appc\\.${keyg}${keyh}\\.com|qifu-api\\.baidubce\\.com|dashi\\.163\\.com`
     ),
     PROXIES
   )
@@ -446,6 +446,32 @@ async function getDirectInfo(ip, provider) {
           .filter(i => i)
           .join(' '),
         ['运营商:', $.lodash_get(body, 'data.owner')].filter(i => i).join(' '),
+      ]
+        .filter(i => i)
+        .join('\n')
+    } catch (e) {
+      $.logErr(`${msg} 发生错误: ${e.message || e}`)
+    }
+  } else if (!ip && provider == 'baidu') {
+    try {
+      const res = await http({
+        url: `https://qifu-api.baidubce.com/ip/local/geo/v1/district`,
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.14',
+        },
+      })
+      let body = String($.lodash_get(res, 'body'))
+      try {
+        body = JSON.parse(body)
+      } catch (e) {}
+      const data = body?.data
+      const ip = body?.ip
+      isCN = data?.country === '中国'
+      CN_IP = ip
+      CN_INFO = [
+        ['位置:', isCN ? getflag('CN') : '', data?.prov, data?.city, data?.district].filter(i => i).join(' '),
+        ['运营商:', data?.isp || data?.owner].filter(i => i).join(' '),
       ]
         .filter(i => i)
         .join('\n')
