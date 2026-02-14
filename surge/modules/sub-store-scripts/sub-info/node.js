0d7bda0badb861f853c1d9db8600a39a7315a99d
xream <xream@icloud.com>
2026-02-15 00:30:52 +0800

    feat: Sub-Store 订阅添加流量/过期信息节点脚本同步最新获取订阅流量信息逻辑

--- surge/modules/sub-store-scripts/sub-info/node.js
+++ surge/modules/sub-store-scripts/sub-info/node.js
@@ -1,49 +1,71 @@
 async function operator(proxies = [], targetPlatform, context) {
   let args = $arguments || {}
-  const { parseFlowHeaders, getFlowHeaders, flowTransfer, getRmainingDays } = flowUtils
+  const $ = $substore
+  const { parseFlowHeaders, getFlowHeaders, flowTransfer, getRmainingDays, normalizeFlowHeader } = flowUtils
   const sub = context.source[proxies?.[0]?._subName || proxies?.[0]?.subName]
   let subInfo
-  if (sub.source === 'local' && !['localFirst', 'remoteFirst'].includes(sub.mergeSources)) {
-    if (sub.subUserinfo) {
-      if (/^https?:\/\//.test(sub.subUserinfo)) {
-        subInfo = await getFlowHeaders(undefined, undefined, undefined, sub.proxy, sub.subUserinfo)
-      } else {
-        subInfo = sub.subUserinfo
-      }
-    }
-  } else {
-    let url = `${sub.url}`
-      .split(/[\r\n]+/)
-      .map(i => i.trim())
-      .filter(i => i.length)?.[0]
+  let flowInfo
+  if (sub.source !== 'local' || ['localFirst', 'remoteFirst'].includes(sub.mergeSources)) {
+    try {
+      let url =
+        `${sub.url}`
+          .split(/[\r\n]+/)
+          .map(i => i.trim())
+          .filter(i => i.length)?.[0] || ''
 
-    let urlArgs = {}
-    const rawArgs = url.split('#')
-    url = url.split('#')[0]
-    if (rawArgs.length > 1) {
-      try {
-        // 支持 `#${encodeURIComponent(JSON.stringify({arg1: "1"}))}`
-        urlArgs = JSON.parse(decodeURIComponent(rawArgs[1]))
-      } catch (e) {
-        for (const pair of rawArgs[1].split('&')) {
-          const key = pair.split('=')[0]
-          const value = pair.split('=')[1]
-          // 部分兼容之前的逻辑 const value = pair.split('=')[1] || true;
-          urlArgs[key] = value == null || value === '' ? true : decodeURIComponent(value)
+      let $arguments = {}
+      const rawArgs = url.split('#')
+      url = url.split('#')[0]
+      if (rawArgs.length > 1) {
+        try {
+          // 支持 `#${encodeURIComponent(JSON.stringify({arg1: "1"}))}`
+          $arguments = JSON.parse(decodeURIComponent(rawArgs[1]))
+        } catch (e) {
+          for (const pair of rawArgs[1].split('&')) {
+            const key = pair.split('=')[0]
+            const value = pair.split('=')[1]
+            // 部分兼容之前的逻辑 const value = pair.split('=')[1] || true;
+            $arguments[key] = value == null || value === '' ? true : decodeURIComponent(value)
+          }
         }
       }
+      if (!$arguments.noFlow && /^https?/.test(url)) {
+        // forward flow headers
+        flowInfo = await getFlowHeaders(
+          $arguments?.insecure ? `${url}#insecure` : url,
+          $arguments.flowUserAgent,
+          undefined,
+          sub.proxy,
+          $arguments.flowUrl
+        )
+        if (flowInfo) {
+          const headers = normalizeFlowHeader(flowInfo, true)
+          if (headers?.['subscription-userinfo']) {
+            subInfo = headers['subscription-userinfo']
+          }
+        }
+      }
+    } catch (err) {
+      $.error(`订阅 ${sub.name} 获取流量信息时发生错误: ${JSON.stringify(err)}`)
+      $.error(err?.message)
+      $.error(err?.stack)
     }
-    args = { ...urlArgs, ...args }
-    if (!args.noFlow) {
-      if (sub.subUserinfo) {
-        if (/^https?:\/\//.test(sub.subUserinfo)) {
-          subInfo = await getFlowHeaders(undefined, undefined, undefined, sub.proxy, sub.subUserinfo)
-        } else {
-          subInfo = sub.subUserinfo
-        }
-      } else {
-        subInfo = await getFlowHeaders(url)
+  }
+  if (sub.subUserinfo) {
+    let subUserInfo
+    if (/^https?:\/\//.test(sub.subUserinfo)) {
+      try {
+        subUserInfo = await getFlowHeaders(undefined, undefined, undefined, proxy || sub.proxy, sub.subUserinfo)
+      } catch (e) {
+        $.error(`订阅 ${sub.name} 使用自定义流量链接 ${sub.subUserinfo} 获取流量信息时发生错误: ${JSON.stringify(e)}`)
       }
+    } else {
+      subUserInfo = sub.subUserinfo
+    }
+
+    const headers = normalizeFlowHeader([subUserInfo, flowInfo].filter(i => i).join(';'), true)
+    if (headers?.['subscription-userinfo']) {
+      subInfo = headers['subscription-userinfo']
     }
   }
 
